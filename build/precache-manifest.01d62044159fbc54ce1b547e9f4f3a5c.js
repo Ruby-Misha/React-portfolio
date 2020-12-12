a57565103793b34989c0ffd9b2923ce809c3d634
Ruby-Misha <62019979+Ruby-Misha@users.noreply.github.com>
2020-12-12 08:18:37 -0800

    Add files via upload

---
+++ build/precache-manifest.01d62044159fbc54ce1b547e9f4f3a5c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe26bcaa3746d5348b865082a08c45f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10fe8b19429a1107331c",
+    "url": "/static/css/main.660adf21.chunk.css"
+  },
+  {
+    "revision": "0550eb91c97db8e66437",
+    "url": "/static/js/2.389dd508.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.389dd508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fe8b19429a1107331c",
+    "url": "/static/js/main.2d61137b.chunk.js"
+  },
+  {
+    "revision": "eb80a0ad05127806b236",
+    "url": "/static/js/runtime-main.8255ee5f.js"
+  },
+  {
+    "revision": "9d07141dd9a4b776147d16a109d78dff",
+    "url": "/static/media/facebook.9d07141d.png"
+  },
+  {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/static/media/github.0918d786.png"
+  },
+  {
+    "revision": "8ce47fd872187bfa8d11505b3bd8944e",
+    "url": "/static/media/instagram.8ce47fd8.png"
+  },
+  {
+    "revision": "2a7192e558328a1636d3b239ef294c48",
+    "url": "/static/media/micu.2a7192e5.jpg"
+  },
+  {
+    "revision": "e854e540767a115a8c62da482873a005",
+    "url": "/static/media/resume.e854e540.pdf"
+  }
+]);
